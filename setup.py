ca50b8bda42fad299857e83c326522b6e7c8db23
Chris Lamb <lamby@debian.org>
2008-12-01 23:26:22 +0000

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='django_lint',
+    packages=[
+        'django_lint',
+        'django_lint.management',
+        'django_lint.management.commands',
+    ],
+)
