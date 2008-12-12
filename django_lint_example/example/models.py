23f46cc7e3fe32d313de2cb96e54d55876a38f02
Chris Lamb <lamby@debian.org>
2008-12-12 07:06:34 +0000

    Add indirection to a "null=True" value to show inference at work.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- django_lint_example/example/models.py
+++ django_lint_example/example/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
+TRUE_VALUE = True
+
 class LintModel(models.Model):
     nullable_charfield = models.CharField(max_length=100, null=True)
+    nullable_charfield_2 = models.CharField(max_length=100, null=TRUE_VALUE)
+    charfield = models.CharField(max_length=100)
