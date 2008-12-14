c03487ac3a77096aa684b3d7baa886a1cb476b96
Chris Lamb <lamby@debian.org>
2008-12-14 00:33:13 +0000

    Add some warnings for standard model methods.

--- django_lint/AstCheckers/model_methods.py
+++ django_lint/AstCheckers/model_methods.py
@@ -12,23 +12,65 @@ class ModelMethodsChecker(BaseChecker):
     name = 'django_model_models'
     msgs = {
         'W8010': (
-            'Too many models; consider splitting application',
+            'Too many models (%d/%d); consider splitting application',
         '',),
         'W8011': ('Use __unicode__ instead of __str__', '',),
+        'W8012': ('Method should come after standard model methods', '',),
+        'W8013': ('Standard model method should come before %r', '',),
+        'W8014': ('Missing __unicode__ method', '',),
     }
+
+    options = (
+        ('max-models', {
+            'default': 15,
+            'type': 'int',
+            'metavar': '<int>',
+            'help': 'Maximum number of models per module',
+        }),
+    )
 
     def visit_module(self, node):
         self.model_count = 0
 
     def leave_module(self, node):
-        if self.model_count >= 10:
-            self.add_message('W8010', node=node.root())
+        if self.model_count >= self.config.max_models:
+            self.add_message('W8010', node=node.root(),
+                args=(self.model_count, self.config.max_models))
+
+    def visit_function(self, node):
+        if not is_model(node.parent.frame()):
+            return
+
+        if node.name == '__str__':
+            self.add_message('W8011', node=node)
+
+        try:
+            idx = [
+                '__unicode__',
+                '__str__',
+                'save',
+                'get_absolute_url',
+            ].index(node.name)
+
+            if self.prev_idx == -1:
+                self.add_message('W8012', node=self.prev_node)
+
+            elif idx < self.prev_idx:
+                self.add_message('W8013', node=node, args=self.prev_node.name)
+
+        except ValueError:
+            idx = -1
+
+        self.prev_idx = idx
+        self.prev_node = node
 
     def visit_class(self, node):
         if not is_model(node):
             return
 
-        self.model_count += 1
+        if '__unicode__' not in [x.name for x in node.mymethods()]:
+            self.add_message('W8014', node=node)
 
-        if '__str__' in node.locals:
-            self.add_message('W8011', node=node.locals['__str__'][0])
+        self.model_count += 1
+        self.prev_idx = None
+        self.prev_name = None
