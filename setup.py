4ed823057f7e490fa9e4ef9b1c9f941f625e13e1
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-14 15:50:54 +0100

    Releasing version 2.0.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@
 setup_args = dict(
     name='django-lint',
     url="https://chris-lamb.co.uk/projects/django-lint",
-    version='2',
+    version='2.0.1',
     license="BSD",
     description="Static analysis tool for Django projects.",
     packages=[
